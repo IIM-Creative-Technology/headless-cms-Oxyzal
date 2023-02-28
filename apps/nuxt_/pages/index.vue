ebe60d3fe043c752c34fb8d92ac213f8779f7441
Oxyzal <steven.madi@edu.devinci.fr>
2023-02-28 15:51:50 +0100

    :tada: INIT project with strapi and nuxt

---
+++ apps/nuxt_/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Hello world</h1>
+    <nuxt-link to="/" v-for="project in projects.data" :key="project.id"> 
+        <pre>{{  project.name }}</pre>
+    </nuxt-link>
+</template>
+
+<script setup>
+    const {find} = useStrapi();
+    const projects = await find('projects', {populate:'deep'});
+    console.log(projects);
+</script>
